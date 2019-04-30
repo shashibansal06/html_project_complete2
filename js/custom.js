e20cafc0eb39017671a0888ca0726e3e392e20a5
Shashi <shashi@ignivasolutions.com>
2019-04-30 15:48:57 +0530

    design

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+function toglrnav() {
+    var x = document.getElementById("custom-nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function closenav() {
+    var x = document.getElementById("custom-nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function sidebartoglrnav() {
+    var x = document.getElementById("sidenav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
